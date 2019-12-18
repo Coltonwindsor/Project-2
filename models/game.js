6b9fe81e474e659ecaca773113f41945ccbc2d41
Colton Windsor <Coltonwindsor@yahoo.com>
2019-12-18 12:01:51 -0500

    set up models

--- models/game.js
+++ models/game.js
@@ -1,57 +1,40 @@
-/* 
- * Place all functions, classes, and/or DB schemas here for a single 
- * model.
- */
+const mongoose = require('./connection.js')
 
-/* Step 1
- *
- * TODO: import mongoose connection
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const mongoose = require('./connection.js')
+const GameSchema = new mongoose.Schema({
+  name: String,
+  releaseYear: Number,
+  genre: String,
+  cover: String
+})
 
-/* Step 1 alternative
- *
- * TODO: make a global variable to act as an in memory database. 
- * NOTE: doing this WILL NOT persist your data and you will loose
- * your data once you stop running your server.
- *
- */
-global.sampleModel = [];
+const GameCollection = mongoose.model('game', GameSchema)
 
-/* Step 2
- *
- * TODO: create model schema 
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const SampleModelSchema = new mongoose.Schema({
-//  name: String
-//})
-
-/* Step 3
- *
- * TODO: create collection API
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const SampleCollection = mongoose.model('Sample', SampleModelSchema)
-
-/* Step 4
- *
- * TODO: delete this it's just a sample
- *
- */
-function getHelloWorldString() {
-  return 'hello world'
+const getGameById = (id) => {
+  return GameCollection.findById(id)
 }
 
-/* Step 5
- *
- * TODO: export all functions from this file by adding their names as keys to this
- * object
- */
+const getAllGames = () => {
+  return GameCollection.find({})
+}
+
+
+const createGame = (newGame) => {
+  return GameCollection.create(newGame)
+}
+
+const updateGame = (id, updatedGame) => {
+  return GameCollection.updateOne({ _id: id }, updatedGame)
+}
+
+const deleteGame = (id) => {
+  return GameCollection.deleteOne({ _id: id })
+}
+
+
 module.exports = {
-  getHelloWorldString
+  getGameById,
+  getAllGames,
+  createGame,
+  updateGame,
+  deleteGame,
 }
